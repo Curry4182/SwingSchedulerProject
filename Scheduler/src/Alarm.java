eb6413e650e322b93a72d187baed158c6d26ee41
kkcchh9999 <kkcchh9999@naver.com>
2021-12-02 15:50:03 +0900

    API call and Alarm

--- Scheduler/src/Alarm.java
+++ Scheduler/src/Alarm.java
@@ -1,6 +1,14 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.swing.*;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 
 public class Alarm implements Runnable {
 	private ArrayList<Schedule> allSchedule; //사용자가 추가한 모든 일정 정보를 관리한다.
@@ -9,6 +17,7 @@ public class Alarm implements Runnable {
 	private ArrayList<String> holidayText; //공휴일 문구를 저장한다.
 	private int alarmState; //사용자가 설정한 알림 상태를 저장한다, 1이면 울리고, 0이면 울리지 않는것같음
 	private Thread th; //알림발생시간에 알림 팝업을 발생시키는 스레드이다.
+
 
 	//알림 OnOff 를 위한 쓰레드를 동작할 Int, KCH
 	static int threadOnOff = 0;
@@ -47,19 +56,30 @@ public class Alarm implements Runnable {
 		//알림설정 KCH
 		threadOnOff = 1;
 		while (threadOnOff == 1) {
-			//현재시간 받아서 처리하는 로직
+			//NowDate
 			Date date = new Date(System.currentTimeMillis());
-			String dateString = date.toString();
-			String[] dateArr = dateString.split("[: ]");
+			//DateFormat
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm");
+			String dateString = simpleDateFormat.format(date);
+			//Split String, Struct ex: [2021, 12, 02, 16, 00]
+			String[] dateArr = dateString.split("[.]");
+			//current Time, ex: 1600
 			int currentTime = Integer.parseInt(dateArr[3] + dateArr[4]);
-			//dateArr 에 다음과 같은 데이터 포함 : Wed Dec 01 16 09 17 KST 2021
-			if (true) {	//공휴일 API 땡겨와서 비교해야함!! KCH
-				alertHolidayAlarm();
-			} else {
-				//현재시간과 저장된 시간들 비교해서 일치하면 알림발생
-				for (int i : alarmTime){
-					if (alarmTime.get(i) == currentTime) {
-						alertActivityAlarm();
+			//call API with Year	Call here Temporary!!!! CHECK FOR ALARM!!!!
+			crawlingHolidayInf(dateArr[0]);
+			//current Day, ex: 20211201
+			String nowDate = dateArr[0] + dateArr[1] + dateArr[2];
+			for (int j: holidayDate) {	//Compare Holiday
+				if (Objects.equals(holidayDate.get(j), Integer.valueOf(nowDate))) {
+					if (currentTime == 850) {	//08:50 Holiday Alarm
+						alertHolidayAlarm(j);	//j for alert user what holiday is it
+					}
+				} else {
+					//compare time, and alert
+					for (int i : alarmTime){
+						if (alarmTime.get(i) == currentTime) {
+							alertActivityAlarm();
+						}
 					}
 				}
 			}
@@ -67,7 +87,7 @@ public class Alarm implements Runnable {
 	}
 
 	//Seq(5) 알림발생 관련 KCH ------------------------------------------------------
-	//사용자가 알림을 on으로 설정했을 때 호출되는 함수이다. 알림 스레드를 실행한다.
+	//사용자가 알림을 on 으로 설정했을 때 호출되는 함수이다. 알림 스레드를 실행한다.
 	public void startAlarmSystem() {
 		ArrayList <Integer> startTimeList =	getAllStartTime();
 		run();
@@ -101,13 +121,44 @@ public class Alarm implements Runnable {
 	}
 	
 	//사용자에게 공휴일 문구를 출력한 알림 팝업을 띄운다. 
-	public void alertHolidayAlarm() {
-		//알림은 JOptionPane 이용하라고 하네 KCH
-		JOptionPane.showMessageDialog(null, "일정 시간, 일정 이름", "활동 알림", JOptionPane.PLAIN_MESSAGE);
+	public void alertHolidayAlarm(int j) {
+		JOptionPane.showMessageDialog(null, "Today is " + holidayText.get(j), "Holiday Alarm", JOptionPane.PLAIN_MESSAGE);
 	}
 	
-	//공휴일 정보를 크롤링하여 공휴일 문구를 holidayText에 저장한다. 
-	public void crawlingHolidayInf() {
+	//Use Holiday API to get holiday, input Year (ex: "2021")
+	public void crawlingHolidayInf(String year) {
+		//API URL
+		String url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo?serviceKey=W8ehC9X8gh1OEHqMZh92XTL%2BOOkcr9Q3mErTlyrS%2Bh9qRF0CP9SGn6c4DdrJMaWPkKz5Ln1oT4RhyAT6A1EZcA%3D%3D&numOfRows=50&solYear="+year;
+		try {
+			Document document = DocumentBuilderFactory
+					.newInstance()
+					.newDocumentBuilder()
+					.parse(url);
+			document.getDocumentElement().normalize();
+			NodeList nodeList = document.getElementsByTagName("item");
+			holidayDate = null;	//reset data
+			holidayText = null;	//reset data
+			for (int i = 0; i<nodeList.getLength(); i++) {
+				Node node = nodeList.item(i);
+
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element)node;
+					holidayText.add(getTagValue("dateName", element));	//insert data, format ex: "Christmas"
+					holidayDate.add(Integer.valueOf(Objects.requireNonNull(getTagValue("locdate", element))));	//insert arr data, format ex: 20211225
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	//Used to get Holiday in crawlingHolidayInf method
+	private static String getTagValue(String tag, Element element) {
+		NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+		Node node = (Node)nodeList.item(0);
+		if (node == null) {
+			return null;
+		}
+		return node.getNodeValue();
 	}
 	
 	//allSchedule의 getAllStartTime함수에서 반환받은 값을 alarmTime에 저장한다. 
