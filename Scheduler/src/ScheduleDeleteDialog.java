6e5b00414e280e6a4867d84f820c15a95f4730f7
병곤 강 <kangbk4182@naver.com>
2021-12-02 13:23:05 +0900

    .

---
+++ Scheduler/src/ScheduleDeleteDialog.java
@@ -0,0 +1,73 @@
+
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import java.awt.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ScheduleDeleteDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	public ScheduleDeleteDialog(String content, ActionListener action) {
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setVisible(true);
+		setBounds(100, 100, 250, 170);
+		
+		final Toolkit toolkit = Toolkit.getDefaultToolkit();
+		final Dimension screenSize = toolkit.getScreenSize();
+		final int x = (screenSize.width - getWidth()) / 2;
+		final int y = (screenSize.height - getHeight()) / 2;
+		setLocation(x, y);
+		
+		setTitle("상세정보");
+		//getContentPane().setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new GridLayout(1, 0, 0, 0));
+		{
+			JLabel lblNewLabel = new JLabel(content);
+			lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("삭제");
+				okButton.addActionListener(action);
+				okButton.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+		        });
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("취소");
+				cancelButton.addActionListener(new ActionListener() {
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								setVisible(false);
+							}
+				        });
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
